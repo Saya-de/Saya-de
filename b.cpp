e5364c345b7531a229e0181a0477a5017bc1e329
Saya <92759471+Saya-de@users.noreply.github.com>
2022-12-23 10:27:11 +0100

    Update b.cpp

--- b.cpp
+++ b.cpp
@@ -2,3 +2,47 @@
 using namespace std;
 
 cout << "your mom" << endl;
+
+#include <iostream>
+using namespace std;
+
+cout << "your mom" << endl;
+#include <iostream>
+using namespace std;
+
+cout << "your mom" << endl;#include <iostream>
+using namespace std;
+
+cout << "your mom" << endl;#include <iostream>
+using namespace std;
+
+cout << "your mom" << endl;
+
+#include <iostream>
+using namespace std;
+
+cout << "your mom" << endl;#include <iostream>
+using namespace std;
+
+cout << "your mom" << endl;#include <iostream>
+using namespace std;
+
+cout << "your mom" << endl;#include <iostream>
+using namespace std;
+
+cout << "your mom" << endl;#include <iostream>
+using namespace std;
+
+cout << "your mom" << endl;#include <iostream>
+using namespace std;
+
+cout << "your mom" << endl;#include <iostream>
+using namespace std;
+
+cout << "your mom" << endl;#include <iostream>
+using namespace std;
+
+cout << "your mom" << endl;#include <iostream>
+using namespace std;
+
+cout << "your mom" << endl;
